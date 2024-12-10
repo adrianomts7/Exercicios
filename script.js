78fe09876d885522616575b0abc1133814bc329d
adrianomts7 <161032614+adrianomts7@users.noreply.github.com>
2024-12-10 12:28:31 -0300

    Portifolio

---
+++ script.js
@@ -0,0 +1,16 @@
+function criaCalculadora(){
+    display = document.querySelector("#display")
+    limpar = document.querySelector(".btn-clear")
+
+    inicia(){
+        this.cliqueBotoes()
+        this.enter()
+    }
+
+    enter(){
+        this.display.addEventListener('keyup', e => {
+            
+        })
+    }
+
+}
