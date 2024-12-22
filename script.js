8acec1cd0bbb3730fec462b1346db2bad419ebc1
adrianomts7 <161032614+adrianomts7@users.noreply.github.com>
2024-12-22 18:31:43 -0300

    Relogio Digital

--- script.js
+++ script.js
@@ -1,60 +1,17 @@
-function Calculadora() {
+function Relogio(){
     let display = document.querySelector('.display')
 
-    this.inicia = () => {
-        this.capturaCliques()
-        this.pressionarEnter()
-    }
+    let data = new Date()
 
-    this.capturaCliques = () => {
-        document.addEventListener('click', event => {
-            const e = event.target
+    let horaAtual = data.toLocaleString('pt-BR', {
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+    })
 
-            if(e.classList.contains('btn-num')) this.adicionarTela(e)
-            if(e.classList.contains('btn-clear')) this.limpaTela()
-            if(e.classList.contains('btn-del')) this.apagarNumero()    
-            if(e.classList.contains('btn-cal')) this.realizaConta()
-        })
-    }
-
-    this.pressionarEnter = () => {
-        document.addEventListener('keypress', e => {
-            if(e.keyCode === 13) this.realizaConta()
-        })
-    }
-
-    this.adicionarTela = (e) => {
-        display.value += e.innerText
-        display.focus()
-    }
-
-    this.limpaTela = () => {
-        display.value = ' '
-    }
-
-    this.apagarNumero = () => {
-        display.value = display.value.slice(0, -1)
-    }
-
-    this.realizaConta = () => {
-        try{
-            const conta = eval(display.value)
-
-            if(!conta){
-                alert('Conta Invalida')
-                return this.limpaTela()
-            }
-
-            this.limpaTela()
-            display.value = String(conta)
-        }
-        catch{
-            alert('Conta Invalida')
-            this.limpaTela()
-        }
-    }
+    display.innerHTML = horaAtual
 
 }
 
-const calcula = new Calculadora()
-calcula.inicia()
+setInterval(Relogio, 1000)
+Relogio()
