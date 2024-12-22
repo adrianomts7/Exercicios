83331e1a88f0cf7a28addb16457b7a9abf8c81b7
adrianomts7 <161032614+adrianomts7@users.noreply.github.com>
2024-12-22 17:23:12 -0300

    Calculadora feita com auxilio de pesquisas e videos

--- script.js
+++ script.js
@@ -1,16 +1,53 @@
-function criaCalculadora(){
-    display = document.querySelector("#display")
-    limpar = document.querySelector(".btn-clear")
+function Calculadora(){
+    let display = document.querySelector('.display');
 
-    inicia(){
-        this.cliqueBotoes()
-        this.enter()
+    this.inicia = () => {
+        this.capturaCliques()
     }
 
-    enter(){
-        this.display.addEventListener('keyup', e => {
-            
+    this.capturaCliques = () => {
+        document.addEventListener('click', event => {
+            const e = event.target
+
+            if(e.classList.contains('btn-num')) this.adicionarNumeroDisplay(e)
+            if(e.classList.contains('btn-clear')) this.limparTela()
+            if(e.classList.contains('btn-cal')) this.realizaConta()
+            if(e.classList.contains('btn-del')) this.apagarNumero()
+
         })
     }
 
+    this.limparTela = () => {
+        display.value = ' '
+    }
+
+    this.adicionarNumeroDisplay = (valor) => {
+        display.value += valor.innerText
+    }
+
+    this.apagarNumero = () => {
+        display.value = display.value.slice(0, -1)
+    }
+
+    this.realizaConta = () => {
+        try{
+            const conta = eval(display.value)
+            
+            if(!conta){
+                alert('Conta Invalida!')
+                return this.limparTela()
+            }
+
+            this.limparTela()
+            display.value = String(conta)
+        }
+        catch{
+            alert('Conta Invalida!')
+            this.limparTela()
+        }
+    }
+
 }
+
+const calcula = new Calculadora()
+calcula.inicia()
