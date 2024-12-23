6066a15d1377e17b717f35c1325effda1318da9c
adrianomts7 <161032614+adrianomts7@users.noreply.github.com>
2024-12-23 11:56:32 -0300

    Lista de Tarefas feita com auxilio de videos e pesquisas

--- script.js
+++ script.js
@@ -1,17 +1,79 @@
-function Relogio(){
-    let display = document.querySelector('.display')
+const inputTarefa = document.querySelector('.input-tarefa')
+const btnTarefa = document.querySelector('.btn-tarefa')
+const tarefas = document.querySelector('.tarefas')
 
-    let data = new Date()
+function criaLi(){
+    const li = document.createElement('li')
+    return li
+}
 
-    let horaAtual = data.toLocaleString('pt-BR', {
-        hour: "2-digit",
-        minute: "2-digit",
-        second: "2-digit",
-    })
+inputTarefa.addEventListener('keypress', e => {
+    if(e.keyCode === 13){
+        if(!inputTarefa.value) return
+        criaTarefa(inputTarefa.value)
+        limpaInput()
+    }
+})
 
-    display.innerHTML = horaAtual
+function limpaInput(){
+    inputTarefa.value = ' '
+    inputTarefa.focus()
+}
+
+function criaBotaoApagar(li){
+    li.innerText += ' '
+    const botaApagar = document.createElement('button')
+    botaApagar.innerText = 'Apagar'
+    botaApagar.setAttribute('class', 'apagar')
+    li.appendChild(botaApagar)
+}
+
+function criaTarefa(textoInput){
+    const li = criaLi()
+    li.innerText = textoInput
+    tarefas.appendChild(li)
+    limpaInput()
+    criaBotaoApagar(li)
+    salvarTarefas()
+}
+
+btnTarefa.addEventListener('click', () => {
+    if(!inputTarefa.value) return
+    criaTarefa(inputTarefa.value)
+})
+
+document.addEventListener('click', event => {
+    const e = event.target
+
+    if(e.classList.contains('apagar')){
+        e.parentElement.remove()
+        salvarTarefas()
+    }
+
+})
+
+function salvarTarefas(){
+    const liTarefas = tarefas.querySelectorAll('li')
+    const listaTarefas = []
+
+    for(let tarefa of liTarefas){
+        let tarefaTexto = tarefa.innerText
+        tarefaTexto = tarefaTexto.replace('Apagar', '').trim()
+        listaTarefas.push(tarefaTexto)
+    }
+
+    const tarefasJSON = JSON.stringify(listaTarefas)
+    localStorage.setItem('tarefas', tarefasJSON)
+}
+
+function adicionarTarefaSalvas(){
+    const tarefa = localStorage.getItem('tarefas')
+    const listaTarefas = JSON.parse(tarefas)
+
+    for(let tarefa of listaTarefas){
+        criaTarefa(tarefa)
+    }
 
 }
 
-setInterval(Relogio, 1000)
-Relogio()
+adicionarTarefaSalvas()
