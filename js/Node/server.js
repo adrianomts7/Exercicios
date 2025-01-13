53c0c5792cadd8849a8443b44efffa286889ae02
adrianomts7 <161032614+adrianomts7@users.noreply.github.com>
2025-01-13 15:23:09 -0300

    Base do express

--- js/Node/server.js
+++ js/Node/server.js
@@ -10,23 +10,45 @@ mongoose.connect(process.env.url_db)
     })
     .catch(e => console.log(e))
 
+const session = require('express-session')
+const MongoStore = require('connect-mongo')
+const flash = require('connect-flash')
+
 const route = require('./routes')
 const path = require('path')
-const {dadosCliente, helloWorld} = require('./src/middlewares/middleware')
+const {dadosCliente, checkCsurfError, csrfMiddleware} = require('./src/middlewares/middleware')
+const helmet = require('helmet')
+const csurf = require('csurf')
 
 app.use(express.urlencoded({extended: true}))
 app.use(express.static(path.resolve(__dirname, 'public')))
+
+const sessionOptions = session({
+    secret: 'Testando',
+    store: MongoStore.create({mongoUrl: process.env.url_db}),
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        // 3 dias em milessimos de sgundos
+        maxAge: 1000 * 60 * 60 * 24 * 3,
+        httpOnly: true
+    }
+})
+
+app.use(sessionOptions)
+app.use(csurf())
 
 app.set('views', path.resolve(__dirname, 'src', 'views'))
 app.set('view engine', 'ejs')
 
 app.use(dadosCliente)
+app.use(checkCsurfError)
+app.use(csrfMiddleware)
 app.use(route)
 
 app.on('pronto', () => {
-
-})
-app.listen(3000, () => {
-    console.log('Acesse ao  terminal: http://localhost:3000')
-    console.log('Servidor inicializado com sucesso')
+    app.listen(3000, () => {
+        console.log('Acesse ao  terminal: http://localhost:3000')
+        console.log('Servidor inicializado com sucesso')
+    })
 })
--- js/Node/src/middlewares/middleware.js
+++ js/Node/src/middlewares/middleware.js
@@ -6,3 +6,14 @@ exports.dadosCliente = (req, res, next) => {
     
     next()
 }
+
+exports.checkCsurfError = (err, req, res, next) => {
+    if(err & 'EBADCSRFTOKEN' === err.code){
+        return res.render('404')
+    }
+}
+
+exports.csrfMiddleware = (req, res, next) => {
+    res.locals.csrfToken = req.csrfToken()
+    next()
+}
