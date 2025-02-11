0aa63078f315e92b825a1b2921ac61d5ac26d722
adrianomts7 <161032614+adrianomts7@users.noreply.github.com>
2025-02-11 18:07:00 -0300

    Estilizando completa da tela de login e do cadastro

--- js/Node/routes.js
+++ js/Node/routes.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const route = express.Router()
 const loginControllers = require('./src/controllers/loginControllers')
+const registerControllers = require('./src/controllers/registerControllers')
 
 // Login
 route.get('/login', loginControllers.index)
+
+// Register
+route.get('/register', registerControllers.index)
 
 module.exports = route
--- js/Node/src/controllers/loginControllers.js
+++ js/Node/src/controllers/loginControllers.js
@@ -3,7 +3,3 @@ const ContatoModel = require('../models/contatomodels')
 exports.index = (req, res) => {
     res.render('login')
 }
-
-exports.trataPost = (req, res) => {
-    return res.send('Dados recebido com sucesso!')
-}
